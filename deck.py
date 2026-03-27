7ec5fb900d08f64acfcb409b45b61ad72dbaccd1
tristanstern <thstern.ieu2024@student.ie.edu>
2026-03-27 12:42:11 +0100

    New Session

--- deck.py
+++ deck.py
@@ -1,13 +1,14 @@
 import random
+
 class PlayingCard:
-    SUITS = ["♠", "♡", "♦", "♤", "♣"]
-    RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    SUITS = ["♠", "♥", "♦", "♣"]
+    RANKS= ["1", "2", "3", "4", "5" "6", "7", "8", "9", "10", "J", "K", "Q", "A"]
     def __init__(self, suit, rank):
         if suit not in self.SUITS:
-            raise ValueError("Invalid suit")
+            raise ValueError("ivalid suit")
         if rank not in self.RANKS:
-            raise ValueError("Invalid rank")
-        self._suit = suit
+            raise ValueError("invalid rank")
+        self._suit= suit
         self._rank = rank
 
     @property
@@ -16,39 +17,37 @@ class PlayingCard:
 
     @property
     def rank(self):
-        return self._rank
+        return self.rank
 
     def __str__(self):
-        return f"{self.suit} {self.rank}"
+        return f"{self._rank}{self._suit}"
     def __repr__(self):
         return self.__str__()
 
-class Deck:
-    """
-    Deck of cards, 52 of PlayingCard
+class Deck :
+    """"
+    Deck of cards, 52 of playing cards
     """
     def __init__(self):
-        self._cards = []
+        self._cards= []
         for suit in PlayingCard.SUITS:
             for rank in PlayingCard.RANKS:
-                card = PlayingCard(suit, rank)
+                card= PlayingCard(suit, rank)
                 self._cards.append(card)
 
     def __str__(self):
         return str(self._cards)
     def shuffle(self):
+        #mix cards in the deck
         random.shuffle(self._cards)
 
     def deal(self):
-        #take out first card from the deck
+        #take out hr first card from the deck
         return self._cards.pop(0)
-
-card = PlayingCard("♠", "2")
-card2 = PlayingCard("♣", "A")
-print(card)
-print(card2)
-deck = Deck()
-print(deck)
-deck.shuffle()
-print(deck)
-print(deck.deal())
+if __name__== "__main__":
+    card= PlayingCard("♠", "2")
+    print(card)
+    deck=Deck()
+    print(deck)
+    print(deck)
+    print(deck.deal())
--- poker.py
+++ poker.py
@@ -1,20 +1,56 @@
 from deck import Deck
 class PokerHand:
+    """"
+    class representing a pokeck hand
+    we will wornk yo rank the hand and determn trips full house, 4 of a kid,etc
     """
-    Class representing a poker hand
-    We will work to rank the hand and determine trips, full houses, 4 of a kind, etc...
-    """
-
     def __init__(self):
-        deck = Deck()
+        deck= Deck()
         deck.shuffle()
-        _cards = []
+        _cards= []
         for i in range(5):
-            _cards.append(deck.deal())
+             _cards.append(deck.deal())
         self._cards = _cards
 
     def __str__(self):
         return str(self._cards)
 
-hand = PokerHand()
-print(hand)
+    @property
+    def is_flush(self):
+        first_card_suit= self._cards[0].suit
+        for i in range (1, 5):
+            if first_card_suit!= self._cards[i].suit:
+                return False
+        return True
+    @property
+    def matches(self):
+        #nedd to compare ech card against all the others
+        matches= 0
+        for card1 in self._cards:
+            if card1==card2:
+                continue
+            if card1.rank== card2.rank:
+                matches +=1
+        return matches
+
+
+    @property
+    def is_fullhouse(self):
+        return self.matches== 8
+
+
+
+iterations = 0
+hits = 0
+while True:
+    hand= PokerHand()
+    iterations +=1
+    if hand.is_flush:
+        #print(hand)
+        #print("Done it in ", iterations)
+        hits +=1
+    if hits == 1000:
+            prob = hits/ iterations*100
+            print(f"probability of flush is {prob}%")
+            break
+
